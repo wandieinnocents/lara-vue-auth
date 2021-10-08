ccd95ff97e1c9c13fe6d414b786c8210ff4a70c0
wandieinnocents <wandieinnocent2@gmail.com>
2021-10-08 16:10:05 +0300

    Initial commit

---
+++ resources/views/assets/js/app.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import Home from "./components/Home.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [{
+        path: "/",
+        name: "home",
+        component: Home
+    }]
+});
+
+new Vue({
+    el: "#app",
+    router: router,
+    render: app => app(App)
+});
